b5649470475faf7386d2354da3e6e4a60ac53c2d
laissugeda <la_sugeda@hotmail.com>
2021-12-07 11:14:35 -0300

    inclusoes no layout

--- gameTracker/src/app/app.module.ts
+++ gameTracker/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './pages/principal/principal.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
