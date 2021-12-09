6d3424d004719a3e9068ff0fe69fe44bbd2da091
Fatec <you@example.com>
2021-12-09 16:12:35 -0200

    implementacoes no form

--- adm/src/app/pages/cadastro-ofertas/cadastro-ofertas.component.ts
+++ adm/src/app/pages/cadastro-ofertas/cadastro-ofertas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { OfertasService } from 'src/app/shared/ofertas.service';
 
 @Component({
   selector: 'app-cadastro-ofertas',
@@ -7,15 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroOfertasComponent implements OnInit {
 
+  form: FormGroup
+
   lojas = [
-    { id: 1, nome: 'Epic' },
-    { id: 2, nome: 'Origin' },
-    { id: 3, nome: 'Steam' },
+    { id: 1, nome: 'Epic', valor: 5 },
+    { id: 2, nome: 'Origin', valor: 2 },
+    { id: 3, nome: 'Steam', valor: 7 },
   ];
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder, 
+    private service: OfertasService
+    ) { }
 
   ngOnInit(): void {
+    this.form = this.fb.group({
+      gameID: [null, [Validators.required]],
+      normalPrice: [null, [Validators.required]],
+      salePrice: [null, [Validators.required]],
+      title: [null, [Validators.required]],
+      storeID: [null, [Validators.required]]
+    })
+  }
+
+  salvar(){
+    console.log(this.form.value)
+    if (this.form.valid){
+      this.service.criar(this.form.value).subscribe(
+        success => alert("Oferta criada com sucesso!"),
+        error => alert(error)
+      )
+      console.log("salvarrrr")
+    }
   }
 
 }
