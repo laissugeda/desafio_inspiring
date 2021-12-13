ba74f08105b87d4446fe95dce316af8b9d01243e
Fatec <you@example.com>
2021-12-13 15:27:22 -0200

    implementacao nos filtros - filtrar item pesquisado

--- gameTracker/src/app/pages/principal/principal.component.ts
+++ gameTracker/src/app/pages/principal/principal.component.ts
@@ -16,6 +16,7 @@ export class PrincipalComponent implements OnInit {
   soma = 12 //item por página
   option = "% de Descontos"
   elementoPesquisa!: string
+  //resultado!: any[]
 
   constructor(
     private service: GamesService,
@@ -27,8 +28,12 @@ export class PrincipalComponent implements OnInit {
     this.service.getPrincipal().subscribe(res => {
       this.ofertas = res
       //ordena por desconto e fecha select
-      this.ordenaPor("% de Desconto")
-      this.abreSelect()
+      this.ofertas.sort(function (a, b) {
+        let a1 = 100 - (Math.round(a.salePrice*100/a.normalPrice))
+        let b1 = 100 - (Math.round(b.salePrice*100/b.normalPrice))
+        return (a1 < b1) ? 1 : ((b1 < a1) ? -1 : 0);
+      })
+      this.pageSize = this.ofertas.slice(0, this.soma)  
       }
     )  
   }
@@ -64,13 +69,14 @@ export class PrincipalComponent implements OnInit {
   }
 
   pesquisa(event: Event) {
-    this.elementoPesquisa = (event.target as HTMLInputElement).value;
+    this.elementoPesquisa = (event.target as HTMLInputElement).value //retorna valor do input
     let resultado = []
     for (let game of this.ofertas){
+      // verifica se a palavra pesquisada (em letras minusulas e sem espaços) existe no titulo de algum jogo
       if (game.title.trim().toLowerCase().includes(this.elementoPesquisa)){
-        resultado.push(game)
+        resultado.push(game) // this.resultado recebe os jogos que contem o elemento no titulo
       }
-
+      //mensagem de erro caso this.resultado seja vazio 
       if(this.elementoPesquisa.length > 2 && resultado.length == 0){
         this.snackBar.open("Jogo não encontrado!", " ", {duration: 3000})
       }
@@ -81,43 +87,51 @@ export class PrincipalComponent implements OnInit {
 
 // método de filtros
   ordenaPor(filtro: string){
+    let conteudo
+    //verifica o conteudo a ser filtrado (resultado de pesquisa, ou conteudo geral)
+    if(this.elementoPesquisa){
+      conteudo = this.pageSize
+    }
+    else{
+      conteudo = this.ofertas
+    }
     switch(filtro){
       case "maior_preco":
         this.option = "Maior preço"
-        this.ofertas.sort(function (a, b) {
+        conteudo.sort(function (a, b) {
           return a.salePrice - b.salePrice //coloca em ordem crescente
         })
-        this.ofertas.reverse() // inverte a ordem, deixando decrescente
-        this.pageSize = this.ofertas.slice(0, this.soma)  // atualiza a ordem dos cards sem perder a quantidade por pagina
+        conteudo.reverse() // inverte a ordem, deixando decrescente
+        this.pageSize = conteudo.slice(0, this.soma)  // atualiza a ordem dos cards sem perder a quantidade por pagina
         this.abreSelect()
         break
 
       case "menor_preco":
         this.option = "Menor preço"
-        this.ofertas.sort(function (a, b) {
+        conteudo.sort(function (a, b) {
           return a.salePrice - b.salePrice
         })
-        this.pageSize = this.ofertas.slice(0, this.soma)  
+        this.pageSize = conteudo.slice(0, this.soma)  
         this.abreSelect()
         break
 
       case "titulo":
         this.option = "Título"
-        this.ofertas.sort(function (a, b) {
+        conteudo.sort(function (a, b) {
           return (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0);
         })
-        this.pageSize = this.ofertas.slice(0, this.soma)  
+        this.pageSize = conteudo.slice(0, this.soma)  
         this.abreSelect()
         break
 
       default:
         this.option = "% de Desconto"
-        this.ofertas.sort(function (a, b) {
+        conteudo.sort(function (a, b) {
           let a1 = 100 - (Math.round(a.salePrice*100/a.normalPrice))
           let b1 = 100 - (Math.round(b.salePrice*100/b.normalPrice))
           return (a1 < b1) ? 1 : ((b1 < a1) ? -1 : 0);
         })
-        this.pageSize = this.ofertas.slice(0, this.soma)  
+        this.pageSize = conteudo.slice(0, this.soma)  
         this.abreSelect()
     }
   }
